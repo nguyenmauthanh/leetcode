87656edd67ce3f2445922349cd03ce4346b13aa7
thanhnm <thanhnm2@elcom.com.vn>
2023-02-16 16:45:06 +0700

    thanhnm - findpivotindex

--- src/com/nmtc9zz/FindPivotIndex.java
+++ src/com/nmtc9zz/FindPivotIndex.java
@@ -1,4 +1,23 @@
 package com.nmtc9zz;
 
 public class FindPivotIndex {
+    public static void main(String[] args) {
+        int[] array = {1,7,3,6,5,6};
+        System.out.println(pivotIndex(array));
+    }
+
+    public static int pivotIndex(int[] nums){
+        int totalNums = 0;
+        int leftTotalNums = 0;
+
+        for(int element : nums){
+            totalNums += element;
+        }
+        for(int i = 0; i < nums.length ; leftTotalNums += nums[i++]){
+            if(leftTotalNums * 2 == (totalNums - nums[i])){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
