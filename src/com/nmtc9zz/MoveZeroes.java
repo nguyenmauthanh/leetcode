b6b4e1541e9b773000c3452d27f2a23999810106
nguyenmauthanh <nguyenmauthanh21@gmail.com>
2025-01-06 23:33:30 +0700

    283

--- src/com/nmtc9zz/MoveZeroes.java
+++ src/com/nmtc9zz/MoveZeroes.java
@@ -2,10 +2,19 @@ package com.nmtc9zz;
 
 public class MoveZeroes {
     public static void main(String[] args) {
-
+        moveZeroes(new int[]{0, 1, 0, 3, 2});
     }
 
-    public void moveZeroes(int[] nums){
+    public static void moveZeroes(int[] nums){
+        int index = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] != 0){
+                nums[index++] = nums[i];
+            }
+        }
 
+        for(int i = index; i < nums.length ; i++){
+            nums[i] = 0;
+        }
     }
 }
