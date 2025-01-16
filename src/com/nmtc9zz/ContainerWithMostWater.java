7857c20c314c86a94310689b6def48b66f059b7d
nguyenmauthanh <nguyenmauthanh21@gmail.com>
2025-01-17 00:08:30 +0700

    11

---
+++ src/com/nmtc9zz/ContainerWithMostWater.java
@@ -0,0 +1,28 @@
+package com.nmtc9zz;
+
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+        System.out.println(maxArea(new int[]{1, 1}));
+    }
+
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int currentArea = Math.min(height[left], height[right]) * width;
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
