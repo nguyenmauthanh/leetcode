396da73c7d76357b063cefd1ffd3ea7aa1a67065
nguyenmauthanh <nguyenmauthanh21@gmail.com>
2025-01-19 01:18:48 +0700

    1679

--- src/com/nmtc9zz/MaxNumberOfKSumPairs.java
+++ src/com/nmtc9zz/MaxNumberOfKSumPairs.java
@@ -1,15 +1,22 @@
 package com.nmtc9zz;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class MaxNumberOfKSumPairs {
     public static void main(String[] args) {
-        System.out.println(maxOperations(new int[]{1,2,3,4}, 5));
-        System.out.println(maxOperations(new int[]{3,1,3,4,3}, 6));
-        System.out.println(maxOperations(new int[]{2,5,4,4,1,3,4,4,1,4,4,1,2,1,2,2,3,2,4,2}, 3));
+        System.out.println(maxOperationsUseHashMap(new int[]{1,2,3,4}, 5));
+        System.out.println(maxOperationsUseHashMap(new int[]{3,1,3,4,3}, 6));
+        System.out.println(maxOperationsUseHashMap(new int[]{2,5,4,4,1,3,4,4,1,4,4,1,2,1,2,2,3,2,4,2}, 3));
+
+        System.out.println(maxOperationsUseTwoPointer(new int[]{1,2,3,4}, 5));
+        System.out.println(maxOperationsUseTwoPointer(new int[]{3,1,3,4,3}, 6));
+        System.out.println(maxOperationsUseTwoPointer(new int[]{2,5,4,4,1,3,4,4,1,4,4,1,2,1,2,2,3,2,4,2}, 3));
     }
 
-    public static int maxOperations(int[] nums, int k) {
+    // Use Hash Map
+    public static int maxOperationsUseHashMap(int[] nums, int k) {
+        long startTime = System.nanoTime();
         HashMap<Integer, Integer> map = new HashMap<>();
         int res = 0;
 
@@ -23,7 +30,32 @@ public class MaxNumberOfKSumPairs {
                 map.put(num, map.getOrDefault(num, 0) + 1);
             }
         }
+        long endTime = System.nanoTime();
+        System.out.println("maxOperationsUseHashMap:" + (endTime - startTime));
+        return res;
+    }
 
+    // Use Algo Two Pointer
+    public static int maxOperationsUseTwoPointer(int[] nums, int k) {
+        long startTime = System.nanoTime();
+        Arrays.sort(nums);
+        int left = 0, right = nums.length - 1;
+        int res = 0;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == k) {
+                res++;
+                left++;
+                right--;
+            } else if (sum < k) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        long endTime = System.nanoTime();
+        System.out.println("maxOperationsUseTwoPointer:" + (endTime - startTime));
         return res;
     }
 }
