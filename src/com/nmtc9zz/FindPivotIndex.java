34853deeb78d9424a3c4386cc07d640c9ac568a4
nguyenmauthanh <nguyenmauthanh21@gmail.com>
2025-01-20 23:59:26 +0700

    724

--- src/com/nmtc9zz/FindPivotIndex.java
+++ src/com/nmtc9zz/FindPivotIndex.java
@@ -2,21 +2,15 @@ package com.nmtc9zz;
 
 public class FindPivotIndex {
     public static void main(String[] args) {
-        int[] array = {1,7,3,6,5,6};
-        System.out.println(pivotIndex(array));
+        System.out.println(pivotIndex(new int[]{1,7,3,6,5,6}));
     }
 
-    public static int pivotIndex(int[] nums){
-        int totalNums = 0;
-        int leftTotalNums = 0;
-
-        for(int element : nums){
-            totalNums += element;
-        }
-        for(int i = 0; i < nums.length ; leftTotalNums += nums[i++]){
-            if(leftTotalNums * 2 == (totalNums - nums[i])){
-                return i;
-            }
+    public static int pivotIndex(int[] nums) {
+        int sum = 0, leftsum = 0;
+        for (int x: nums) sum += x;
+        for (int i = 0; i < nums.length; ++i) {
+            if (leftsum == sum - leftsum - nums[i]) return i;
+            leftsum += nums[i];
         }
         return -1;
     }
